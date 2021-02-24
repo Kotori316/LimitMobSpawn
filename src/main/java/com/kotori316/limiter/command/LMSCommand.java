e9237d8ddc4d6eb1a5f6968bd8e86a6126e4ccfc
Kotori316 <Kotori316@gmail.com>
2021-02-25 00:16:16 +0900

    Command improved.

--- src/main/java/com/kotori316/limiter/command/LMSCommand.java
+++ src/main/java/com/kotori316/limiter/command/LMSCommand.java
@@ -1,15 +1,21 @@
 package com.kotori316.limiter.command;
 
+import java.util.Set;
+
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import javax.annotation.Nonnull;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 
 import com.kotori316.limiter.LimitMobSpawn;
+import com.kotori316.limiter.TestSpawn;
 import com.kotori316.limiter.capability.Caps;
 import com.kotori316.limiter.capability.LMSHandler;
 
@@ -21,20 +27,45 @@ public class LMSCommand {
         LiteralArgumentBuilder<CommandSource> literal = Commands.literal(LimitMobSpawn.MOD_ID);
         {
             // query
-            literal.then(Commands.literal("query").executes(context -> {
-                World world = context.getSource().getWorld();
-                LMSHandler lmsHandler = world.getCapability(Caps.getLmsCapability()).orElseThrow(HandlersNotFound::create);
-                if (lmsHandler.getDefaultConditions().isEmpty() && lmsHandler.getForceConditions().isEmpty() && lmsHandler.getDenyConditions().isEmpty())
-                    throw RulesNotFound.create();
-                context.getSource().sendFeedback(new StringTextComponent("Defaults"), true);
-                lmsHandler.getDefaultConditions().stream().map(Object::toString).map(StringTextComponent::new).forEach(c -> context.getSource().sendFeedback(c, true));
-                context.getSource().sendFeedback(new StringTextComponent("Denies"), true);
-                lmsHandler.getDenyConditions().stream().map(Object::toString).map(StringTextComponent::new).forEach(c -> context.getSource().sendFeedback(c, true));
-                context.getSource().sendFeedback(new StringTextComponent("Forces"), true);
-                lmsHandler.getForceConditions().stream().map(Object::toString).map(StringTextComponent::new).forEach(c -> context.getSource().sendFeedback(c, true));
+            LiteralArgumentBuilder<CommandSource> query = Commands.literal("query");
+            query.then(Commands.literal("default").executes(context -> {
+                LMSHandler lmsHandler = getLmsHandler(context);
+                sendMessage(context, "Defaults", lmsHandler.getDefaultConditions());
                 return Command.SINGLE_SUCCESS;
             }));
+            query.then(Commands.literal("deny").executes(context -> {
+                LMSHandler lmsHandler = getLmsHandler(context);
+                sendMessage(context, "Denies", lmsHandler.getDenyConditions());
+                return Command.SINGLE_SUCCESS;
+            }));
+            query.then(Commands.literal("force").executes(context -> {
+                LMSHandler lmsHandler = getLmsHandler(context);
+                sendMessage(context, "Forces", lmsHandler.getForceConditions());
+                return Command.SINGLE_SUCCESS;
+            }));
+            literal.then(query
+                .executes(context -> {
+                    LMSHandler lmsHandler = getLmsHandler(context);
+                    sendMessage(context, "Defaults", lmsHandler.getDefaultConditions());
+                    sendMessage(context, "Denies", lmsHandler.getDenyConditions());
+                    sendMessage(context, "Forces", lmsHandler.getForceConditions());
+                    return Command.SINGLE_SUCCESS;
+                }));
         }
         dispatcher.register(literal);
+    }
+
+    private static void sendMessage(CommandContext<CommandSource> context, String s, Set<TestSpawn> conditions) {
+        context.getSource().sendFeedback(new StringTextComponent(s + "=" + conditions.size()), true);
+        conditions.stream().map(Object::toString).map(StringTextComponent::new).forEach(c -> context.getSource().sendFeedback(c, true));
+    }
+
+    @Nonnull
+    private static LMSHandler getLmsHandler(CommandContext<CommandSource> context) throws CommandSyntaxException {
+        World world = context.getSource().getWorld();
+        LMSHandler lmsHandler = world.getCapability(Caps.getLmsCapability()).orElseThrow(HandlersNotFound::create);
+        if (lmsHandler.getDefaultConditions().isEmpty() && lmsHandler.getForceConditions().isEmpty() && lmsHandler.getDenyConditions().isEmpty())
+            throw RulesNotFound.create();
+        return lmsHandler;
     }
 }
