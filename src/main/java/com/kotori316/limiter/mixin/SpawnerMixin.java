ad08ceccba1b6ffae6d2a0f53129c59189bfd6a3
Kotori316 <Kotori316@gmail.com>
2021-06-10 22:29:05 +0900

    Reduced number of modifying parameters.

--- src/main/java/com/kotori316/limiter/mixin/SpawnerMixin.java
+++ src/main/java/com/kotori316/limiter/mixin/SpawnerMixin.java
@@ -6,15 +6,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.kotori316.limiter.LimitMobSpawn;
 import com.kotori316.limiter.capability.Caps;
 import com.kotori316.limiter.capability.LMSHandler;
 import com.kotori316.limiter.capability.SpawnerControl;
 
 @Mixin(AbstractSpawner.class)
 public abstract class SpawnerMixin {
-    private int lmsDefaultSpawnCount, lmsDefaultMaxNearbyEntities;
+    private int lmsDefaultSpawnCount, lmsDefaultMaxNearbyEntities; // default 0
+    private int lmsCacheSpawnCount, lmsCacheMaxNearbyEntities; // default 0
     @Shadow
     private int spawnCount;
     @Shadow
@@ -23,28 +26,75 @@ public abstract class SpawnerMixin {
     @Shadow
     public abstract World shadow$getWorld();
 
-    @Inject(method = "tick", at = @At("HEAD"), allow = 1)
-    public void tickHead(CallbackInfo ci) {
+    @SuppressWarnings("SpellCheckingInspection")
+    @Inject(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/world/spawner/AbstractSpawner;spawnCount:I"), allow = 1)
+    public void spawnCountHead(CallbackInfo ci) {
+        LimitMobSpawn.LOGGER.info("spawnCountHead v {} {}, d {} {}, c {} {}",
+            spawnCount, maxNearbyEntities, lmsDefaultSpawnCount, lmsDefaultMaxNearbyEntities, lmsCacheSpawnCount, lmsCacheMaxNearbyEntities);
         if (!shadow$getWorld().isRemote() && spawnCount == 4) {
             // 4 is the default spawn count. If not 4, the spawner is modifies by others.
             lmsDefaultSpawnCount = spawnCount;
-            lmsDefaultMaxNearbyEntities = maxNearbyEntities;
-            shadow$getWorld().getCapability(Caps.getLmsCapability())
-                .resolve()
-                .map(LMSHandler::getSpawnerControl)
-                .flatMap(SpawnerControl::getSpawnCount)
-                .ifPresent(newCount -> {
-                    spawnCount = newCount; //Math.min(newCount, defaultSpawnCount);
-                    maxNearbyEntities = Math.max(spawnCount, lmsDefaultMaxNearbyEntities);
-                });
+            if (lmsCacheSpawnCount == 0) {
+                shadow$getWorld().getCapability(Caps.getLmsCapability())
+                    .resolve()
+                    .map(LMSHandler::getSpawnerControl)
+                    .flatMap(SpawnerControl::getSpawnCount)
+                    .ifPresent(newCount -> spawnCount = newCount);
+                lmsCacheSpawnCount = spawnCount;
+            } else {
+                spawnCount = lmsCacheSpawnCount;
+            }
         }
     }
 
-    @Inject(method = "tick", at = @At("RETURN"))
-    public void tickTail(CallbackInfo ci) {
-        if (!shadow$getWorld().isRemote() && spawnCount == 4) {
+    @SuppressWarnings("SpellCheckingInspection")
+    @Inject(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/world/spawner/AbstractSpawner;spawnCount:I", shift = At.Shift.AFTER))
+    public void spawnCountTail(CallbackInfo ci) {
+        LimitMobSpawn.LOGGER.info("spawnCountTail v {} {}, d {} {}, c {} {}",
+            spawnCount, maxNearbyEntities, lmsDefaultSpawnCount, lmsDefaultMaxNearbyEntities, lmsCacheSpawnCount, lmsCacheMaxNearbyEntities);
+        if (!shadow$getWorld().isRemote() && lmsDefaultSpawnCount == 4) {
             spawnCount = lmsDefaultSpawnCount;
-            maxNearbyEntities = lmsDefaultMaxNearbyEntities;
+            lmsDefaultSpawnCount = 0;
         }
+    }
+
+    @SuppressWarnings("SpellCheckingInspection")
+    @Inject(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/world/spawner/AbstractSpawner;maxNearbyEntities:I"), allow = 1)
+    public void maxNearbyEntitiesHead(CallbackInfo ci) {
+        LimitMobSpawn.LOGGER.info("maxNearbyEntitiesHead v {} {}, d {} {}, c {} {}",
+            spawnCount, maxNearbyEntities, lmsDefaultSpawnCount, lmsDefaultMaxNearbyEntities, lmsCacheSpawnCount, lmsCacheMaxNearbyEntities);
+        if (!shadow$getWorld().isRemote() && maxNearbyEntities == 6) {
+            lmsDefaultMaxNearbyEntities = maxNearbyEntities;
+
+            if (lmsCacheMaxNearbyEntities == 0) {
+                shadow$getWorld().getCapability(Caps.getLmsCapability())
+                    .resolve()
+                    .map(LMSHandler::getSpawnerControl)
+                    .flatMap(SpawnerControl::getSpawnCount)
+                    .ifPresent(newCount -> maxNearbyEntities = Math.max(newCount, lmsDefaultMaxNearbyEntities));
+                lmsCacheMaxNearbyEntities = maxNearbyEntities;
+            } else {
+                maxNearbyEntities = lmsCacheMaxNearbyEntities;
+            }
+        }
+    }
+
+    @SuppressWarnings("SpellCheckingInspection")
+    @Inject(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/world/spawner/AbstractSpawner;maxNearbyEntities:I", shift = At.Shift.AFTER))
+    public void maxNearbyEntitiesTail(CallbackInfo ci) {
+        LimitMobSpawn.LOGGER.info("maxNearbyEntitiesTail v {} {}, d {} {}, c {} {}",
+            spawnCount, maxNearbyEntities, lmsDefaultSpawnCount, lmsDefaultMaxNearbyEntities, lmsCacheSpawnCount, lmsCacheMaxNearbyEntities);
+        if (!shadow$getWorld().isRemote() && lmsDefaultMaxNearbyEntities == 6) {
+            maxNearbyEntities = lmsDefaultMaxNearbyEntities;
+            lmsDefaultMaxNearbyEntities = 0;
+        }
+    }
+
+    @SuppressWarnings("SpellCheckingInspection")
+    @Inject(method = "tick", at = @At("RETURN"),
+        slice = @Slice(from = @At(value = "FIELD", target = "Lnet/minecraft/world/spawner/AbstractSpawner;spawnCount:I")))
+    public void tickTail(CallbackInfo ci) {
+        lmsCacheSpawnCount = 0;
+        lmsCacheMaxNearbyEntities = 0;
     }
 }
