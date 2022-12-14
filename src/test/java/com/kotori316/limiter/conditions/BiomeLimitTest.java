9965bae840ba29b650d870ecc9021f645a617e25
Kotori316 <Kotori316@gmail.com>
2022-12-14 16:11:09 +0900

    Ignore biome test.

--- src/test/java/com/kotori316/limiter/conditions/BiomeLimitTest.java
+++ src/test/java/com/kotori316/limiter/conditions/BiomeLimitTest.java
@@ -6,24 +6,43 @@ import java.util.stream.Stream;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeGenerationSettings;
+import net.minecraft.world.level.biome.BiomeSpecialEffects;
+import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import com.kotori316.limiter.BeforeAllTest;
+import com.kotori316.limiter.LimitMobSpawn;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Disabled("The biome is now registered dynamically so we can't use in unit test.")
 class BiomeLimitTest extends BeforeAllTest {
     static final BiomeLimit NOT_EXIST_BIOME = new BiomeLimit(new ResourceLocation("lms:not_exist_biome"));
 
-    static Stream<Object[]> registeredBiomes() {
-        return ForgeRegistries.BIOMES.getEntries().stream()
-            .map(e -> new Object[]{e.getKey(), e.getValue()});
+    static Stream<Arguments> registeredBiomes() {
+        var key = ResourceKey.create(ForgeRegistries.Keys.BIOMES, new ResourceLocation(LimitMobSpawn.MOD_ID, "test_biome"));
+        var biome = new Biome.BiomeBuilder()
+            .precipitation(Biome.Precipitation.NONE)
+            .temperature(0.5F)
+            .downfall(0.5F)
+            .specialEffects(new BiomeSpecialEffects.Builder()
+                .waterColor(0).waterFogColor(0).fogColor(0).skyColor(0)
+                .build())
+            .mobSpawnSettings(new MobSpawnSettings.Builder().build())
+            .generationSettings(BiomeGenerationSettings.EMPTY)
+            .temperatureAdjustment(Biome.TemperatureModifier.NONE)
+            .build();
+
+        return Stream.of(Arguments.of(key, biome));
     }
 
     @Test
